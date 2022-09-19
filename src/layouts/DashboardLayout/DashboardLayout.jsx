bcd8c076b9ac5dcb562d8d25e354998972adadf0
Versha <versha.thakur@kelltontech.com>
2022-09-19 19:53:55 +0530

    push in components

--- src/layouts/DashboardLayout/DashboardLayout.jsx
+++ src/layouts/DashboardLayout/DashboardLayout.jsx
@@ -1,9 +1,11 @@
 import { Box } from "@mui/material";
 import { Outlet } from "react-router-dom";
+import DrawerLeft from "../../components/Nav/Nav";
 
 const DashboardLayout = () => {
   return (
     <Box sx={{ px: ['15px', '20px', '30px', '60px'], pb: ['15px', '20px', '30px', '60px'], pt: { xs: '60px', md: '80px' } }}>
+       <DrawerLeft />
       <Outlet />
     </Box>
   );
