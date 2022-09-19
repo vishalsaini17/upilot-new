fd175e8f85710c31617933b2994a6049b439b30e
Vishal Saini <vishal.saini@kellton.com>
2022-09-19 12:37:48 +0530

    update

--- src/layouts/CheckBox/CheckBox.jsx
+++ src/layouts/CheckBox/CheckBox.jsx
@@ -1,11 +1,11 @@
 import Checkbox from '@mui/material/Checkbox';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormGroup from '@mui/material/FormGroup';
-import * as React from 'react';
+import { useState } from 'react';
 
 
 export default function IndeterminateCheckbox() {
-  const [checked, setChecked] = React.useState([true, false]);
+  const [checked, setChecked] = useState([true, false]);
 
   const handleChange = (event) => {
     setChecked([event.target.checked, event.target.checked]);
@@ -14,7 +14,7 @@ export default function IndeterminateCheckbox() {
   return (
     <FormGroup>
       <FormControlLabel control={<Checkbox defaultChecked OnChange={handleChange} />} label="Remember Me" />
-     
+
     </FormGroup>
   );
 }
