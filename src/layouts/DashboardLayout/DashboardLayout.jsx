0520ead40ec71963a246c44e76d1f08be5d8997e
Vishal Saini <vishal.saini@kellton.com>
2022-09-20 19:33:11 +0530

    update nav

--- src/layouts/DashboardLayout/DashboardLayout.jsx
+++ src/layouts/DashboardLayout/DashboardLayout.jsx
@@ -1,15 +1,16 @@
 import { Box } from "@mui/material";
+import { Outlet } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
 
 const DashboardLayout = () => {
   return (
-    <Box>
+    <Box sx={{ px: ['15px', '20px', '30px', '60px'], pb: ['15px', '20px', '30px', '60px'], pt: { xs: '60px', md: '80px' } }}>
        {/* <DrawerLeft /> */}
        <Navbar />
-      {/* <Outlet /> */}
+       
+      <Outlet />
     </Box>
   );
 }
 
 export default DashboardLayout;
-// sx={{ px: ['15px', '20px', '30px', '60px'], pb: ['15px', '20px', '30px', '60px'], pt: { xs: '60px', md: '80px' } }}
